7164586911cad55b917301e8cbd7cc086d0a09cc
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-06-03 12:53:01 -0400

    added final sequelized files

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,40 +1,80 @@
 // require express and the burgers model
-var express = require('express');
-var burgers = require('../models/burgers.js');
+var db = require('../models');
+var Burger = db.Burger;
 
-// export routes
+// export the routes
 module.exports = function(app) {
-	// get root
-	app.get('/', function(request, response) {
-		burgers.allBurgers(function(data) {
-			response.render('index', {
-				wholeBurgers: data.whole,
-				devouredBurgers: data.devoured
-			});
-		});
-	});
+    // get the root route
+    app.get('/', function(request, response) {
+        var object = {};
+        Burger.findAll({
+            where: {
+                devoured: false
+            }
+        }).then(function(result) {
+            object.uneatenBurgers = result;
+            return;
+        }).then(function() {
+          return Burger.findAll({
+              where: {
+                  devoured: true
+              }
+          });
+        }).then(function(result) {
+            object.eatenBurgers = result;
+            return;
+        }).then(function() {
+            response.render('index', {
+                uneatenBurgers: object.uneatenBurgers,
+                eatenBurgers: object.eatenBurgers
+            }); 
+        });
+    });
 
-	//api/burgers route all data
-	app.get('/api/burgers', function(request, response) {
-		burgers.allBurgers(function(data) {
-			response.json(data);
-		});
-	});
+    // define the get api/burgers route - for all burger data
+    app.get('/api/burgers', function(request, response) {
+        Burger.findAll({}).then(function(result) {
+            response.json(result);
+        });
+    });
 
-	//create burger
-	app.post('/', function(request, response) {
-		var newBurger = request.body.burger;
-		if(newBurger === '') {
-			response.redirect('/');
-			return;
-		}
-		burger.create(newBurger, function() {
-			response.redirect('/');
-		});
-	});
+    // define post for creating a burger
+    app.post('/', function(request, response) {
+        var newBurger = request.body.burger;
+        // if no burger is defined just return
+        if (newBurger === '') {
+            response.redirect('/');
+            return;
+        }
+        // create that burger
+        Burger.create({
+            burger_name: newBurger
+        }).then(function() {
+            response.redirect('/');
+        });
+    });
 
-	// update burger
-	app.put('/:id', function(request, response) {
-		response.redirect('/');
-	});
+    // define the get api/burgers/:id route - for single burger data
+    app.get('/api/burgers/:id', function(request, response) {
+        Burger.findOne({
+            where: {
+                id: request.params.id
+            }
+        }).then(function(data) {
+            response.json(data);
+        });
+    });
+
+    // define put for updating a burger
+    app.put('/:id', function(request, response) {
+        Burger.update({
+            devoured: true
+        }, {
+            where: {
+                id: request.params.id
+            }
+        }).then(function() {
+            response.redirect('/');
+        });
+    });
 };
--- server.js
+++ server.js
@@ -1,43 +1,37 @@
-// *********************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
+// require the things
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
+var db = require('./models');
 
-// Sets up the Express App
-// =============================================================
+// set up server
 var app = express();
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-/*app.use(bodyParser.json({ type: "application/vnd.api+json" }));*/
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+        console.log('listening on ' + port);
+    });
+});
 
-// Static directory
-app.use(express.static("app/public"));
+// set up handlebars engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Serve static content for the app from the 'public' directory in the
+// application directory.
+app.use(express.static(__dirname + '/public'));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Routes
-// =============================================================
-require("./controllers/burger_controller.js")(app);
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+// get them routes
+require('./controllers/burger_controller.js')(app);
